cb020cde554d3bd798ca4cd1d0ca5663eb38efb6
shale <slucas001@citymail.cuny.edu>
2024-10-30 16:21:21 -0400

    implemented Redirected, fixed syntax error and afixed us to um to better match type abbrev

--- src/main.go
+++ src/main.go
@@ -3,27 +3,44 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
+	// "time"
 )
 
 type URLMap struct {
-	urls map{string}string
+	urls map[string]string
 }
 
-func (us *URLMap) Shorten (writer http.ResponseWriter, req *http.Request) {
+func (um *URLMap) Shorten (writer http.ResponseWriter, req *http.Request) {
 
 }
 
-func (us *URLMap) Redirected (writer http.ResponseWriter, req *http.Request) {
+func (um *URLMap) Redirected (writer http.ResponseWriter, req *http.Request) {
+	
+	shortKey := req.URL.Path[len("/shortened/"):]
+	if shortKey == "" {
+		http.Error(writer, "The shortened key is missing", http.StatusBadRequest)
+		return
+	}	
 
+	longURL, found := um.urls[shortKey]
+	if !found {
+		http.Error(writer, "The shortened key wasn't fount", http.StatusNotFound)
+		return
+	}
+
+	http.Redirect(writer, req, longURL, http.StatusMovedPermanently)
 }
 
 func main () {
+
 	shortener := &URLMap {
 		urls: make(map[string]string),
 	}
-
+	
 	http.HandleFunc("/link", shortener.Shorten)
 	http.HandleFunc("/shortened/", shortener.Redirected)	
+
+	fmt.Println("Started server on port 8080.")
 	http.ListenAndServe(":8080", nil)
+
 }
