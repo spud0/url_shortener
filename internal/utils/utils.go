d812d54170efba192798c2c3e7716a1daf3cc848
shale <slucas001@citymail.cuny.edu>
2024-11-01 17:01:09 -0400

    removed the scaffolding from the utils file

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,8 +1,6 @@
 package internal
 
 import (
-	"database/sql"
-	"github.com/lib/pq"
 	"crypto/md5"
 	"strings"
 	"encoding/base64"
@@ -21,34 +19,8 @@ func MakeHash (str string) string {
 	return strings.Trim(genUniqueHash(str), "==")
 }
 
-
-// Caching scaffolding
-type Cache struct {
-	Client *redis.Client
-}
-
-func (c *Cache) CacheURL () {
-
-}
-
-func (c *Cache) GetURLFromCache () {
-
-}
-
-
-// database scaffolding
-type DB struct {
-	Conn *sql.DB
-}
-
-func (db *DB) SaveURL (url string) (bool, error) {
-	
-}
-
-func (db *DB) GetURL (url string) (string, error) {
-
-}
-	
+/* 
 func Nothing () int {
 	return 3434;
 }
+*/
