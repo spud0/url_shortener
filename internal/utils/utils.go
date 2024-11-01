a4b42287da67980fc87df01fc42379460c189705
shale <slucas001@citymail.cuny.edu>
2024-11-01 12:11:33 -0400

    added some type of unique hash (not actually unique)

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -6,12 +6,15 @@ import (
 	"crypto/md5"
 	"strings"
 	"encoding/base64"
-
 )
 
 // Hashing scaffolding
 func genUniqueHash(str string) string {
-	
+	md5Hasher := md5.New()
+	md5Hasher.Write([]byte(str))
+	hash :=	md5Hasher.Sum(nil)
+	return base64.StdEncoding.EncodeToString(hash[:5])
+
 }
 
 func MakeHash (str string) string {
