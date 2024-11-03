4673ed220a26e5ed4bd865ae0609ce0a063930c6
shale <slucas001@citymail.cuny.edu>
2024-11-02 23:23:35 -0400

    some comments and removing useless things

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -6,21 +6,15 @@ import (
 	"encoding/base64"
 )
 
-// Hashing scaffolding
+// Creates a unique hash, but isn't really unique b/c of truncation ... 
 func genUniqueHash(str string) string {
 	md5Hasher := md5.New()
 	md5Hasher.Write([]byte(str))
 	hash :=	md5Hasher.Sum(nil)
-	return base64.StdEncoding.EncodeToString(hash[:5])
-
+	return base64.StdEncoding.EncodeToString(hash[:8])
 }
 
+// Public function
 func MakeHash (str string) string {
 	return strings.Trim(genUniqueHash(str), "==")
 }
-
-/* 
-func Nothing () int {
-	return 3434;
-}
-*/
