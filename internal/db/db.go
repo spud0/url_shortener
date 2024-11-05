ffac04998c62d672800af4c94eca17983d2ff398
shale <slucas001@citymail.cuny.edu>
2024-11-04 23:45:33 -0500

    working on setting up the db

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,13 +2,36 @@ package internal
 
 import (
 	"database/sql"
-	"github.com/lib/pq"
+	_ "github.com/lib/pq"
+	"os"
+	"fmt"
 )
 
 // database scaffolding
 // Need to figure out the DB schema
 type DB struct {
 	Conn *sql.DB
+}
+
+// Initializes a new connection to the database.
+func (db *DB) newDB ()  (*DB, error) {
+	
+	// Getting the environment variables
+	port, dbName := os.Getenv("PORT"), os.Getenv("NAME")
+	user, passwd:= os.Getenv("USER"), os.Getenv("PASS")
+	hostName := os.Getenv("HOST")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			hostName, port, user, passwd, dbName)
+
+	conn, err := sql.Open("postgres", psqlInfo)
+	if (err != nil) {
+		return nil, err	
+	}
+
+	fmt.Println("connected to db")
+	return &DB{Conn: conn}, nil
+	
 }
 
 func (db *DB) SaveURL (url string) error {
